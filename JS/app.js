9f6087c6235ef4d7de69ed8e7ce0e71966d23fa5
khaledABUZENAH <djkw365@gmail.com>
2021-04-27 16:19:06 +0300

    new changes

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,8 @@ let userName= prompt('welcome to my webpage, please enter your name');
 
 alert('welcome ' + userName);
 
+let score=0
+
 let age= prompt('do I have 24 years old?');
 console.log(age.toUpperCase())
 switch(age.toUpperCase()){
@@ -15,6 +17,7 @@ switch(age.toUpperCase()){
         case 'N':
         //console.log('good job, i\"m 22');
     alert('good job, i\"m 22');
+    score++;
     break; 
 
 
@@ -27,6 +30,7 @@ switch(favTeam.toUpperCase()){
         case 'Y':
             //console.log ('you are right, GGMU');
         alert('you are right, GGMU');
+        score++;
         break;
     case 'NO':
         case 'N':
@@ -48,6 +52,7 @@ switch(adress.toUpperCase()){
         case 'N':
         // console.log('well done, I live in Rusayfah');
          alert('well done, I live in Rusayfah');
+         score++;
          break;
 
 }
@@ -59,6 +64,7 @@ switch(major.toUpperCase()){
         case 'Y':
            //console.log('good job');
         alert('good job');
+        score++;
         break;
      case 'NO':
         case 'N':
@@ -80,11 +86,53 @@ switch(hoppy.toUpperCase()){
         case 'N':
             //console.log('you are stunning' + userName);
          alert('you are stunning ' + userName);
-          break;
+         score++;
+         break;
 
         
 }
+ 
+let broSis=Number (prompt('how many brothers and sisters do I have?'));
+for (let i =0; i<4; i++){
+    if(broSis>3){
+
+    broSis=prompt("thats too high");
+    }
+     if (broSis<3){
+
+    broSis=prompt('thats too low');
+    }
+    else if (broSis==3){
+    alert('you nailed it!');
+    score++;
+    break;
+    }
+    
+    }
+
+    let question7= prompt('I like so many colors, guess one of them');
+    let color= ['red', 'blue', 'black', 'green'];
+    for (let i =0; i<6; i++){
+        if (question7== color[0]||question7==color[1] || question7==color[2] || question7==color[3]){
+        alert('correct!');
+        score++;
+        break;
+        }
+
+        else { 
+            question7=prompt('try again');
+         }
+        
+        
+        }
+    
+
+    
+      alert('may fav colors are; red, blue, black, and green');
+      alert('your final score is '+score+ ' out of 7');
+
     
 alert('I hope you enjoyed ' + userName);
+
 
 
